d2efc650ff5fe43d7db337fca91674a47f19f32e
dev-prabhukiran <helloprabhukiran@gmail.com>
2026-01-05 21:33:09 +0530

    Fix: case-sensitive import issue

--- src/constants/PodcastLinks.js
+++ src/constants/PodcastLinks.js
@@ -5,17 +5,17 @@ import Podcast3 from "../assets/images/hqdefault3.jpg";
 export const PODCAST_LINKS = [
   {
     url_id: "dRXq81Om2a4",
-    image:Podcast1,
+    image: Podcast1,
     title: "How a 21-Year-Old Got an ₹80 LPA Offer (Before Degree!)",
   },
   {
     url_id: "PZNgcH2Jtac",
-    image:Podcast2,
+    image: Podcast2,
     title: "$25000 Bug Bounty, $100k Offer as a Solana Engineer",
   },
   {
     url_id: "TbbCJQ4Vbko",
-    image:Podcast3,
+    image: Podcast3,
     title: "How He Cracked A 40Lakhs Remote Coding Job (Complete Story)",
   },
 ];
